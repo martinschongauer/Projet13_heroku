7a47a62c3d9e8dc8c8acaebf58d8a30e923951a2
Baptiste Hornecker <113470794+martinschongauer@users.noreply.github.com>
2023-05-08 18:53:58 +0200

    Update tests.py

--- main_app/tests.py
+++ main_app/tests.py
@@ -4,4 +4,4 @@ from django.urls import reverse
 def test_main_page(client):
     response = client.get(reverse('index'))
     assert response.status_code == 200
-    assert b'<h1>Welcome to Holiday Homes</h1>' in response.content
+    assert b'<h1>Welcome to Holiday Homes - modified</h1>' in response.content
